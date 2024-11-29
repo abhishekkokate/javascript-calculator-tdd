31a5a0cdfb098012fe68e86493b93847716f0e79
abhishek.kokate <abhishekkokate8@gmail.com>
2024-11-29 21:05:20 +0530

    Add function completed to calculate sum of numbers separated by ","

--- calc.js
+++ calc.js
@@ -1,5 +1,29 @@
 function add(numbers) {
-    console.log(numbers);
+    let sum = 0;
+    try {
+        // Splitting the numbers into an array | e.g. "1,2,3" -> [1,2,3]
+        const numbersArr = numbers.trim("").split(",");
+        // Looping on array to calculate the sum
+        numbersArr.map((num) => {
+            // Parsing the number into an integer
+            const parsedNum = parseInt(num);
+            if (isNaN(parsedNum)) {
+                // Throwing an error if the number is not a number
+                throw new Error("Invalid number | Not a number");
+            }
+            // Adding the number to the sum
+            sum += parsedNum;
+        });
+    } catch (err) {
+        console.error(`Error while adding numbers | Error: ${err.message}`);
+        // Reseting sum incase of an error.
+        sum = 0;
+    }
+    return sum;
 }
 
-add("1,2,3");
+const sum1 = add("");
+const sum2 = add("1");
+const sum3 = add("1,5");
+
+console.log({ sum1, sum2, sum3 });
